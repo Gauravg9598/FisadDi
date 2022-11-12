bd8607afaaf50ae9c597c91f1ce1859cb13cc230
Gauravg9598 <61234830+Gauravg9598@users.noreply.github.com>
2022-11-12 16:00:11 +0530

    Updates

---
+++ precache-manifest.10f840a94ba140b35f92b07ad05b45f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58e37204df0e1e7d2f28",
+    "url": "/FisadDi/static/css/main.3811b5eb.chunk.css"
+  },
+  {
+    "revision": "58e37204df0e1e7d2f28",
+    "url": "/FisadDi/static/js/main.58e37204.chunk.js"
+  },
+  {
+    "revision": "002a2879119e0b5d31fb",
+    "url": "/FisadDi/static/js/1.002a2879.chunk.js"
+  },
+  {
+    "revision": "9ac630023a10ff1fb64e",
+    "url": "/FisadDi/static/js/2.9ac63002.chunk.js"
+  },
+  {
+    "revision": "b8aa5159816918de43aa",
+    "url": "/FisadDi/static/js/runtime~main.b8aa5159.js"
+  },
+  {
+    "revision": "7c03653c3c0d9110514a5460e4484123",
+    "url": "/FisadDi/index.html"
+  }
+];
